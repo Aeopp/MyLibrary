f43d6836e46846603c2b38ca79480645bd67c1a8
Aeopp <irmxj777@gmail.com>
2020-10-17 22:06:23 +0900

    TIME 프레임 제한 기능 추가

--- MyLibrary/Time.cpp
+++ MyLibrary/Time.cpp
@@ -1,34 +1,44 @@
 #include "Time.h"
 #include <iostream>
-
-void Time::initialize()
-{
-	PrevTime = std::chrono::high_resolution_clock::now();
-}
-
-void Time::update()
+void Time::Initialize(const uint32_t LimitFrame)
 {
 	using namespace std::chrono_literals;
-	
+
+	PrevTime = std::chrono::high_resolution_clock::now();
+	LimitDelta = 1000ms / (float)LimitFrame;
+}
+
+bool Time::Update()
+{
+	using namespace std::chrono_literals;
+
 	std::chrono::time_point CurrentTime = std::chrono::high_resolution_clock::now();
-	std::chrono::duration<float,std::ratio<1,1000>> Delta = CurrentTime - PrevTime;
+	std::chrono::duration<float, std::ratio<1, 1000>> Delta = CurrentTime - PrevTime;
+
+	if (LimitDelta > Delta)
+		return false;
 
 	SecCheck += Delta;
-	
-	if(SecCheck>= 1000ms)
+
+	if (SecCheck >= 1000ms)
 	{
 		FPS = _FPSCount;
 		_FPSCount = 0;
 		SecCheck = 0ms;
 	}
 	else
-		 ++_FPSCount; 
-	
-	PrevTime = CurrentTime;
-	
-	DeltaTime = (Delta.count() * 0.001f)* TimeScale;
+		++_FPSCount;
 
-	NotificationCheck();
+	PrevTime = CurrentTime;
+
+	DeltaTime = (Delta.count() * 0.001f) * TimeScale;
+
+	return true;
+}
+
+void Time::Render()
+{
+	RenderFPS();
 }
 
 float Time::Delta()
@@ -36,47 +46,52 @@ float Time::Delta()
 	return DeltaTime;
 }
 
-void Time::TimerRegist(const float initial, const float Repeat, const float End, 
-NotifyEventType _NotifyEvent)
+void Time::TimerRegist(const float initial, const float Repeat, const float End,
+	NotifyEventType _NotifyEvent)
 {
-	_NotifyEvents.emplace_back(false,initial,Repeat,End, initial,std::move(_NotifyEvent));
+	_NotifyEvents.emplace_back(false, initial, Repeat, End, initial, std::move(_NotifyEvent));
 }
 
 void Time::NotificationCheck()&
 {
 	// 시작 시간 , 반복 주기 , 종료 시간 , 현재 측정 시간
-	for(auto iter = std::begin(_NotifyEvents);
-	iter!=std::end(_NotifyEvents);)
+	for (auto iter = std::begin(_NotifyEvents);
+		iter != std::end(_NotifyEvents);)
 	{
-		auto& [bInit,Init, Repeat, End, CurrentDelta, Event] = *iter;
+		auto& [bInit, Init, Repeat, End, CurrentDelta, Event] = *iter;
 
 		Init -= DeltaTime;
 		if (Init > 0.f)continue;
-		
-		if(Init<0.f && !bInit)
+
+		if (Init < 0.f && !bInit)
 		{
 			bInit = true;
 			CurrentDelta = -0.1f;
 		}
-		
+
 		CurrentDelta -= DeltaTime;
 		End -= DeltaTime;
 		bool IsNotifyEnd = false;
-		
-		if(CurrentDelta<0.f)
+
+		if (CurrentDelta < 0.f)
 		{
 			IsNotifyEnd |= Event();
 			CurrentDelta = Repeat;
 		}
-		
+
 		IsNotifyEnd |= (End < 0.f);
-		
+
 		if (IsNotifyEnd)
 		{
 			iter = _NotifyEvents.erase(iter);
-			continue; 
+			continue;
 		}
-		
+
 		++iter;
 	}
+}
+
+void Time::RenderFPS() const& noexcept
+{
+	
 }
--- MyLibrary/Time.h
+++ MyLibrary/Time.h
@@ -4,30 +4,38 @@
 #include <functional>
 #include <vector>
 
-class Time  : public  singleton_interface<Time>
+class Time : public  singleton_interface<Time>
 {
 public:
 	using NotifyEventType = std::function<bool()>;
-	void initialize();
-	void update();
+	// 프레임 제한 값을 입력해주세요.
+	void Initialize(const uint32_t LimitFrame);
+
+	bool Update();
+	void Render();
 
 	float Delta();
 	float TimeScale{ 1.f };
-	uint32_t FPS{0};
+	uint32_t FPS{ 0 };
 public:
 	// 시작 시간 , 반복 주기 , 종료 시간 , 이벤트
 	// 반환값 true 시 종료 시간 상관 없이 이벤트 삭제
 	// false 시 계속 유지
 	void TimerRegist(const float initial, const float Repeat, const float End,
 		NotifyEventType _NotifyEvent/*람다표현식 or std::bind use*/);
-private:
+public:
+	// 등록된 콜백 함수들에게 통지하고싶은 타이밍에 호출하여주세요.
 	void NotificationCheck()&;
 private:
 	std::chrono::time_point<std::chrono::high_resolution_clock> PrevTime{};
 	std::chrono::duration<float, std::ratio<1, 1000>> SecCheck{};
+	std::chrono::duration<float, std::ratio<1, 1000>> LimitDelta{};
+	std::chrono::duration<float, std::ratio<1, 1000>> CurrentDelta{};
+	std::chrono::steady_clock::time_point CurrentTime{ std::chrono::high_resolution_clock::now() };
 	uint32_t _FPSCount{ 0 };
 	float DeltaTime{ 0.f };
 	// 이벤트 시작여부 , 시작 시간 , 반복 주기 , 종료 시간 , 현재 측정 시간
-	std::list<std::tuple<bool,float, float, float, float, NotifyEventType>> _NotifyEvents;
+	std::list<std::tuple<bool, float, float, float, float, NotifyEventType>> _NotifyEvents;
+private:
+	void RenderFPS()const& noexcept;
 };
-
